303b75ca013464bf09770cbe725154dffd45e1cc
pande-rupesh <rupeshpande2001@gmail.com>
2026-01-31 00:36:56 +0530

    commit

--- GroceryShopping/src/main/resources/static/js/admin/index.js
+++ GroceryShopping/src/main/resources/static/js/admin/index.js
@@ -1,8 +1,6 @@
-// js/admin/index.js
 const toggleBtn = document.getElementById('toggleBtn');
 const sidebar = document.getElementById('sidebar');
 
 toggleBtn.addEventListener('click', () => {
-  console.log("Button clicked!");
   sidebar.classList.toggle('active');
 });
