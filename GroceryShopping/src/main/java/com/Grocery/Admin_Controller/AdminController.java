bb4125ae30fab0659a86ece066c0beb7b83a9d53
pande-rupesh <rupeshpande2001@gmail.com>
2026-02-15 19:04:46 +0530

    add Product in db

--- GroceryShopping/src/main/java/com/Grocery/Admin_Controller/AdminController.java
+++ GroceryShopping/src/main/java/com/Grocery/Admin_Controller/AdminController.java
@@ -1,8 +1,18 @@
 package com.Grocery.Admin_Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.Grocery.model.Product;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/admin")
@@ -12,10 +22,24 @@ public class AdminController {
 	public String index() {
 		return "admin/index";
 	}
-	
+
 	@GetMapping("/addproduct")
-	public String AddProduct()
-	{
+	public String AddProduct() {
 		return "admin/AddProduct";
 	}
+
+	@PostMapping("/addProductinDb")
+	public String addProductinDb(@ModelAttribute Product product,
+	                             @RequestParam("file") MultipartFile file,
+	                             Model model) {
+
+	    System.out.println(product);
+	    System.out.println(file.getOriginalFilename());
+
+	    model.addAttribute("msg", "hello");
+
+	    return "admin/AddProduct";
+	}
+
+
 }
---
+++ GroceryShopping/src/main/java/com/Grocery/model/Product.java
@@ -0,0 +1,78 @@
+package com.Grocery.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String product_name;
+	private String brand_name;
+	private String Category;
+	@Column(columnDefinition = "double(10,2)")
+	private double price;
+	@Column(length = 1000)
+	private String description;
+	private boolean  status;
+	private String Image;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getBrand_name() {
+		return brand_name;
+	}
+	public void setBrand_name(String brand_name) {
+		this.brand_name = brand_name;
+	}
+	public String getCategory() {
+		return Category;
+	}
+	public void setCategory(String category) {
+		Category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getImage() {
+		return Image;
+	}
+	public void setImage(String image) {
+		Image = image;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", product_name=" + product_name + ", brand_name=" + brand_name + ", Category="
+				+ Category + ", price=" + price + ", description=" + description + ", status=" + status + ", Image="
+				+ Image + "]";
+	}
+
+}
